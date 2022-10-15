4541a60bd32da0a8b8ecb27acebd19ae0613e642
haphuong1203 <haphuongminyoongi@gmail.com>
2022-10-15 15:41:04 +0700

    active mq

---
+++ KTPMTH6/src/main/java/com/example/KTPMTH6/Ktpmth6Application.java
@@ -0,0 +1,13 @@
+package com.example.KTPMTH6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktpmth6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ktpmth6Application.class, args);
+	}
+
+}
